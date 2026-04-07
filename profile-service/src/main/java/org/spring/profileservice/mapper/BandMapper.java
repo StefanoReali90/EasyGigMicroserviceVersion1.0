b60831b7a90ae2c2502fbf1b8a60b7f6328d1894
Reptile90 <stefano.reali1990@gmail.com>
2026-04-07 23:18:51 +0200

    Add band dto's mapper

--- profile-service/src/main/java/org/spring/profileservice/mapper/BandMapper.java
+++ profile-service/src/main/java/org/spring/profileservice/mapper/BandMapper.java
@@ -1,4 +1,49 @@
 package org.spring.profileservice.mapper;
 
+import org.mapstruct.*;
+import org.spring.profileservice.dto.BandFullResponse;
+import org.spring.profileservice.dto.BandRegistrationRequest;
+import org.spring.profileservice.dto.BandSearchResponse;
+import org.spring.profileservice.entity.Band;
+import org.spring.profileservice.entity.Genre;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring")
 public interface BandMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "city", ignore = true)
+    @Mapping(target = "genres", ignore = true)
+    @Mapping(target = "imagePath", ignore = true)
+    @Mapping(target = "filepath", ignore = true)
+    Band toEntity(BandRegistrationRequest request);
+
+    @Mapping(target = "cityName", source = "city.name")
+    @Mapping(target = "genres", expression = "java(getPrimaryGenreName(band.getGenres()))")
+    BandFullResponse toFullResponse(Band band);
+
+    @Mapping(target = "cityName", source = "city.name")
+    @Mapping(target = "primarygenres", expression = "java(getPrimaryGenreName(band.getGenres()))")
+    BandSearchResponse toSearchResponse(Band band);
+
+    default List<String> mapGenresToName(List<Genre> genres) {
+        if (genres == null) return null;
+        return genres.stream().map(Genre::getName).collect(Collectors.toList());
+    }
+
+    default String getPrimaryGenreName(List<Genre> genres) {
+        if (genres == null|| genres.isEmpty()) return "N/A";
+        return genres.get(0).getName();
+    }
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "city", ignore = true)
+    @Mapping(target = "genres", ignore = true)
+    void updateBandFromDto(BandRegistrationRequest dto, @MappingTarget Band band);
+
+
+
 }
---
+++ profile-service/src/main/java/org/spring/profileservice/service/BandService.java
@@ -0,0 +1,4 @@
+package org.spring.profileservice.service;
+
+public class BandService {
+}
